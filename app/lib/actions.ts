2d37d1e6d8def51a8d5817d86ea458d8a1d0cce6
gyur1kim <qri1114@gmail.com>
2025-01-27 23:39:12 +0900

    feat: server action
    - form 태그에 action 속성을 이용하면 server action 사용 가능
    - revalidatePath를 이용해 데이터 업데이트
    - redirect를 이용해 페이지간 이동

---
+++ app/lib/actions.ts
@@ -0,0 +1,34 @@
+"use server"; // export된 함수는 서버 액션이다. 이게 있는 파일에서, 사용되지 않은 함수는 자동으로 번들에서 제거된다.
+
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}

43dab6d1862a9f0e800d5c2529f00499141aaf5e
gyur1kim <qri1114@gmail.com>
2025-01-29 16:49:31 +0900

    feat: 메타데이터 동적 설정
    - metadata의 template는 layout에 설정하기
    - 단순 title은 page에 지정하기

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from "next";
+
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
@@ -6,6 +8,10 @@ import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
+
+export const metadata: Metadata = {
+  title: "Invoices",
+};
 
 /**
  * page 컴포넌트는 searchParams를 props로 받을 수 있다.
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
-import "@/app/ui/global.css";
+import { Metadata } from "next";
 
+import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+
+// %s -> 동적으로 변경되는 부분, 해당 페이지에 지정된 title값을 넣음
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
